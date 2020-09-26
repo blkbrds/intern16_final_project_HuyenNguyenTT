fefeb9788e0e69fbf688474cb5659d0cd4432112
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-09-26 10:09:28 +0700

    Fix UI Login

--- FinalProject/Library/Ext/UIImageView.Ext.swift
+++ FinalProject/Library/Ext/UIImageView.Ext.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    // For insert layer in Foreground
+    func addBlackGradientLayerInForeground(frame: CGRect, colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.frame
+        gradient.colors = colors.map { $0.cgColor }
+        self.layer.addSublayer(gradient)
+    }
+    // For insert layer in background
+    func addBlackGradientLayerInBackground(frame: CGRect, colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.frame
+        gradient.colors = colors.map { $0.cgColor }
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
