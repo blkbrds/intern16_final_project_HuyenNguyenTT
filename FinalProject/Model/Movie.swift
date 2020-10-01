54f3cfd0ba066e23bfae1d3c9482ff53b1320950
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-10-01 17:36:58 +0700

    Delete code comment

--- FinalProject/Model/Movie.swift
+++ FinalProject/Model/Movie.swift
@@ -16,19 +16,15 @@ class Movie: Mappable {
     var isBooking: Bool = false
     var releaseDate: String = ""
     
-//    var id: String = ""
-//    var name: String = ""
+    //add propeties for detail
     var ratingCode: String = ""
-//    var thumbnail: String = ""
     var movieTrailer: String = ""
     var genre: String = ""
     var movieEndtime: Int = 0
-//    var releaseDate: String = ""
     var fullDescription: String = ""
     var movieDirector: String = ""
     var movieLanguage: String = ""
     var movieActress: String = ""
-//    var isBooking: Bool = false
     
     required init() {}
     required init?(map: Map) { }
@@ -41,18 +37,14 @@ class Movie: Mappable {
         isBooking <- map["is_booking"]
         releaseDate <- map["release_date"]
         
-//        id <- map["id"]
-//        name <- map["name"]
+        // add map data for detail
         ratingCode <- map["rating_code"]
-//        thumbnail <- map["thumbnail"]
         movieTrailer <- map["movie_trailer"]
         genre <- map["genre"]
         movieEndtime <- map["movie_endtime"]
-//        releaseDate <- map["release_date"]
         fullDescription <- map["full_description"]
         movieDirector <- map["movie_director"]
         movieLanguage <- map["movie_language"]
         movieActress <- map["movie_actress"]
-//        isBooking <- map["is_booking"]
     }
 }
