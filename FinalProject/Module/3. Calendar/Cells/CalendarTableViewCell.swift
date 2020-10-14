912995018d5a2668c1b34bd836a01594a7589041
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-10-14 20:24:00 +0700

    Rename author

--- FinalProject/Module/3. Calendar/Cells/CalendarTableViewCell.swift
+++ FinalProject/Module/3. Calendar/Cells/CalendarTableViewCell.swift
@@ -2,7 +2,7 @@
 //  CalendarTableViewCell.swift
 //  FinalProject
 //
-//  Created by bu on 10/13/20.
+//  Created by Huyen Nguyen T.T.[2] on 10/13/20.
 //  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
 //
 
